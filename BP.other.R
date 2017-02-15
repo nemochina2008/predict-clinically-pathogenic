383da0fdfe7e2c65dd5f676c63710f462758c936
Toby Dylan Hocking <tdhock5@gmail.com>
2017-02-15 13:37:46 -0500

    do not use some features for the multivariate models

--- BP.other.R
+++ BP.other.R
@@ -3,14 +3,20 @@ data.txt.vec <- Sys.glob("data/*.txt")
 BP.other <- list()
 set.seed(1)
 n.folds <- 4
+ignore.features <- c(
+  "integrated_confidence_value",
+  "esp6500siv2_all",
+  "1000g2015aug_all",
+  "SubmitterCategories")
 for(data.txt in data.txt.vec){
   data.dt <- fread(data.txt, na.strings="-")
   data.name <- sub(".txt$", "", basename(data.txt))
+  keep <- sapply(data.dt, is.numeric)
+  keep[names(data.dt) %in% ignore.features] <- FALSE
   BP.other[[data.name]] <- list(
     data=list(
-      feature.mat=as.matrix(data.dt[, sapply(data.dt, is.numeric), with=FALSE]),
+      feature.mat=as.matrix(data.dt[, keep, with=FALSE]),
       label.vec=data.dt$clinvar),
     folds=sample(rep(1:n.folds, l=nrow(data.dt))))
 }
-
 save(BP.other, file="BP.other.RData")
--- BP.other.models.R
+++ BP.other.models.R
@@ -1,10 +1,20 @@
-works_with_R("3.2.3",
+works_with_R("3.3.2",
              data.table="1.9.7",
              missMDA="1.10",
              glmnet="1.9.5",
-             "tdhock/WeightedROC@ef8f35ba7ae85e2995fa66afe13732cebb8b5633",
+             "tdhock/WeightedROC@4dc6e54e248dadd2c60f58b9870fd6b1d7e13eef",
              xgboost="0.4.4",
              party="1.0.13")
+
+not.train.features <- c(
+  "MetaSVM_score",
+  "MetaLR_score",
+  "Eigen-raw",
+  "Eigen-PC-raw",
+  "GenoCanyon_score",
+  "MCAP",
+  "REVEL",
+  "esp6500siv2_all", "1000g2015aug_all")
 
 load("BP.other.RData")
 
@@ -19,7 +29,9 @@ for(test.fold in fold.vec){
   for(train.name in names(BP.other)){
     data.folds.list <- BP.other[[train.name]]
     is.train <- data.folds.list$folds != test.fold
-    train.features <- data.folds.list$data$feature.mat[is.train,]
+    all.train.features <- data.folds.list$data$feature.mat[is.train,]
+    keep <- ! colnames(all.train.features) %in% not.train.features
+    train.features <- all.train.features[, keep]
     nb <- estim_ncpPCA(train.features)
     imputed <- MIPCA(train.features, nb$ncp)
     train.imputed <- imputed$res.imputePCA
@@ -44,31 +56,34 @@ for(test.fold in fold.vec){
         objective="binary:logistic",
         eval_metric="auc"
         )
-      xg.mat <- xgb.DMatrix(train.features, label=train.01, weight=train.weight.vec, missing=NA)
+      xg.mat <- xgb.DMatrix(
+        train.features, label=train.01, weight=train.weight.vec, missing=NA)
       fit.xgboost <- xgb.train(
         params=xg.param,
         data=xg.mat,
         nrounds=50
         )
       fit.forest <- cforest(label ~ ., train.df, weights=train.weight.vec)
-      fit.glmnet <- cv.glmnet(train.imputed, train.labels, train.weight.vec, family="binomial")
+      fit.glmnet <- cv.glmnet(
+        train.imputed, train.labels, train.weight.vec, family="binomial")
       single.features.list <- list()
       for(col.name in colnames(data.folds.list$data$feature.mat)){
-        one.feature <- train.df[, col.name]
-        train.and.weights <- data.table(train.df, weight=train.weight.vec)
-        na.dt <- train.and.weights[is.na(one.feature),]
+        train.and.weights <- data.table(
+          feature=all.train.features[, col.name],
+          label=train.df$label,
+          weight=train.weight.vec)
+        na.dt <- train.and.weights[is.na(feature),]
         na.guess <- if(nrow(na.dt)){
           na.class.weights <- na.dt[, list(weight=sum(weight)), by=label]
           paste(na.class.weights[which.max(weight), label])
         }else{
           "Benign"
         }
-        not.na <- train.and.weights[!is.na(one.feature),]
-        not.na.feature <- not.na[[col.name]]
-        ord <- order(not.na.feature)
+        not.na <- train.and.weights[!is.na(feature),]
+        ord <- order(not.na$feature)
         train.ord <- data.table(
           label=not.na$label,
-          feature=not.na.feature,
+          feature=not.na$feature,
           weight=not.na$weight
           )[ord,]
         train.counts <- dcast(
@@ -85,7 +100,7 @@ for(test.fold in fold.vec){
           thresh.counts[, n.incorrect := cum.FP + cum.FN]
           thresh.row <- thresh.counts[which.min(n.incorrect),]
           thresh <- feature.sign*thresh.row$feature
-          score.vec <- feature.sign*not.na.feature
+          score.vec <- feature.sign*not.na$feature
           pred.vec <- ifelse(
             score.vec <= thresh,
             "Benign", "Pathogenic")
@@ -111,7 +126,8 @@ for(test.fold in fold.vec){
                     test.fold, train.name, weight.name, test.name))
         test.list <- BP.other[[test.name]]
         is.test <- test.list$folds == test.fold
-        test.features <- test.list$data$feature.mat[is.test,]
+        all.test.features <- test.list$data$feature.mat[is.test, ]
+        test.features <- all.test.features[, colnames(train.features)]
         nb <- estim_ncpPCA(test.features)
         test.mipca <- MIPCA(test.features, nb$ncp)
         test.imputed <- test.mipca$res.imputePCA
@@ -153,7 +169,7 @@ for(test.fold in fold.vec){
         for(feature.i in 1:nrow(single.features)){
           one.feature <- single.features[feature.i,]
           feature.name <- paste(one.feature$col.name)
-          test.feature.vec <- test.df[[feature.name]]
+          test.feature.vec <- all.test.features[, feature.name]
           pred.score <- ifelse(
             is.na(test.feature.vec),
             one.feature$na.score,
@@ -191,12 +207,14 @@ for(test.fold in fold.vec){
           ##   geom_point(aes(FPR, TPR), data=pred.dot)+
           ##   geom_path(aes(FPR, TPR), data=roc)
           auc <- WeightedAUC(roc)
-          BP.roc.list[[paste(model.name, test.name, train.name, test.fold, weight.name)]] <-
-            data.table(model.name, test.name, train.name, test.fold, weight.name,
-                       roc)
-          BP.pred.list[[paste(model.name, test.name, train.name, test.fold, weight.name)]] <-
-            data.table(model.name, test.name, train.name, test.fold, weight.name,
-                       auc, pred.dot)
+          key <- paste(
+            model.name, test.name, train.name, test.fold, weight.name)
+          BP.roc.list[[key]] <- data.table(
+            model.name, test.name, train.name, test.fold, weight.name,
+            roc)
+          BP.pred.list[[key]] <- data.table(
+            model.name, test.name, train.name, test.fold, weight.name,
+            auc, pred.dot)
         }#model.name
       }#test.name
     }#weight.name
